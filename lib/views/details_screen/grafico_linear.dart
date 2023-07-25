ce8efa3df4c1b57003b5e88e20c2c0cddbc3a985
Vitor Manoel <vitor.santos@atomicsolutions.com.br>
2023-07-25 08:50:44 -0300

    Alterações no gráfico-adição de zoom.

--- lib/views/details_screen/grafico_linear.dart
+++ lib/views/details_screen/grafico_linear.dart
@@ -1,3 +1,238 @@
+
+import 'dart:convert';
+import 'package:syncfusion_flutter_core/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/services.dart';
+
+late String jsonData;
+late String jsonData1;
+late String jsonData2;
+List<dynamic> predicaoValor = [];
+
+class GraficoLinear extends StatefulWidget {
+  final String cardIndex;
+
+  GraficoLinear(this.cardIndex, {Key? key}) : super(key: key);
+
+  @override
+  _GraficoLinearState createState() => _GraficoLinearState();
+}
+
+class _GraficoLinearState extends State<GraficoLinear> {
+  var inicio;
+  late Map<String, dynamic> view;
+  late Map<String, dynamic> view2;
+  late Map<String, dynamic> view3;
+  double? quant;
+  double? menorValor;
+  double? maiorValor;
+  List<FlSpot> chartData1 = [];
+  List<FlSpot> chartData_media_movel = [];
+  List<FlSpot> chartData_sup_boll = [];
+  List<FlSpot> chartData_inf_boll = [];
+  List<FlSpot> chartData = [];
+  late ZoomPanBehavior _zoomPanBehavior;
+  @override
+  void initState() {
+    _zoomPanBehavior = ZoomPanBehavior(
+                  // zoom com scroll
+                  enableMouseWheelZooming : true,
+                  //zoom com dois toque
+                  enableDoubleTapZooming: true,
+                  // zoom para celular com dois dedos
+                  enablePinching: true,
+                  zoomMode: ZoomMode.xy,
+                    enablePanning: true,
+                  
+                 
+                );
+    super.initState();
+    loadData();
+  }
+
+  Future<void> loadData() async {
+    jsonData = await rootBundle.loadString('asset/Empresas_data/${widget.cardIndex}_cotacoes.json');
+    jsonData1 = await rootBundle.loadString('asset/Empresas_data/${widget.cardIndex}_media_movel.json');
+    jsonData2 = await rootBundle.loadString('asset/Empresas_data/${widget.cardIndex}_bandas_boll.json');
+
+    view = jsonDecode(jsonData);
+    view2 = jsonDecode(jsonData1);
+    view3 = jsonDecode(jsonData2);
+
+    quant = view['Close'].length;
+    for (int a = 0; a < view['Close'].length; a++) {
+      predicaoValor.insert(a, view['Close'][a.toString()]);
+      chartData.insert(a, FlSpot(a.toDouble(), view['Close'][a.toString()]));
+    }
+
+    double contador = 13;
+    for (int a = 0; a < view2['media movel'].length; a++) {
+      chartData_media_movel.insert(a, FlSpot(contador, view2['media movel'][a.toString()]));
+      contador += 1;
+    }
+
+    contador = 13;
+    for (int a = 0; a < view3['banda sup boll'].length; a++) {
+      chartData_sup_boll.insert(a, FlSpot(contador, view3['banda sup boll'][a.toString()]));
+      contador += 1;
+    }
+
+    contador = 13;
+    for (int a = 0; a < view3['banda inf boll'].length; a++) {
+      chartData_inf_boll.insert(a, FlSpot(contador, view3['banda inf boll'][a.toString()]));
+      contador += 1;
+    }
+
+    menorValor = predicaoValor.reduce((valorAtual, elemento) => valorAtual < elemento ? valorAtual : elemento);
+    maiorValor = predicaoValor.reduce((valorAtual, elemento) => valorAtual > elemento ? valorAtual : elemento);
+    maiorValor = double.parse(maiorValor!.toStringAsFixed(0)) + 5;
+    menorValor = double.parse(menorValor!.toStringAsFixed(0)) - 5;
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+       body: SafeArea(child: Center(
+            child: Container(
+              child:/*SfChartTheme(
+                data: SfChartThemeData(
+                        brightness: Brightness.dark, 
+                        backgroundColor: Colors.black87
+                ),
+          
+          child:*/ SfCartesianChart(
+            
+            legend: Legend(isVisible: true,  position: LegendPosition.bottom),
+            primaryXAxis: NumericAxis(
+              interval: 35,
+              
+              title: AxisTitle(text: 'Data'),
+              edgeLabelPlacement: EdgeLabelPlacement.shift,
+            ),
+            primaryYAxis: NumericAxis(
+              interval: 10,
+              title: AxisTitle(text: 'Valor'),
+            ),
+            tooltipBehavior: TooltipBehavior(
+              enable: true,
+              header: '',
+              format: 'point.y',
+              builder: (dynamic data, dynamic point, dynamic series, int pointIndex, int seriesIndex) {
+                final dataX = chartData[pointIndex].x.toInt();
+                final closeValue = view['Close'][dataX.toString()].toStringAsFixed(2);
+                final mediaMovelValue = view2['media movel'][dataX.toString()].toStringAsFixed(2);
+                final supBollValue = view3['banda sup boll'][dataX.toString()].toStringAsFixed(2);
+                final infBollValue = view3['banda inf boll'][dataX.toString()].toStringAsFixed(2);
+
+                return Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 4,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text('Data: ${view['Data'][dataX.toString()]}'),
+                      Text('Close: $closeValue'),
+                      Text('Média Móvel: $mediaMovelValue'),
+                      Text('Banda Sup. Bollinger: $supBollValue'),
+                      Text('Banda Inf. Bollinger: $infBollValue'),
+                    ],
+                  ),
+                );
+              },
+            ),
+            series: <ChartSeries>[
+              LineSeries<FlSpot, double>(
+                dataSource: chartData,
+                xValueMapper: (FlSpot spot, _) => spot.x,
+                yValueMapper: (FlSpot spot, _) => spot.y,
+                color: Colors.blue,
+                name: 'Cotações',
+              ),
+              LineSeries<FlSpot, double>(
+                dataSource: chartData_media_movel,
+                xValueMapper: (FlSpot spot, _) => spot.x,
+                yValueMapper: (FlSpot spot, _) => spot.y,
+                color: Colors.orange,
+                name: 'Média Móvel',
+              ),
+              LineSeries<FlSpot, double>(
+                dataSource: chartData_sup_boll,
+                xValueMapper: (FlSpot spot, _) => spot.x,
+                yValueMapper: (FlSpot spot, _) => spot.y,
+                color: Colors.purple,
+                name: 'Banda Sup. Bollinger',
+              ),
+              LineSeries<FlSpot, double>(
+                dataSource: chartData_inf_boll,
+                xValueMapper: (FlSpot spot, _) => spot.x,
+                yValueMapper: (FlSpot spot, _) => spot.y,
+                color: Colors.purple[300],
+                name: 'Banda Inf. Bollinger',
+              ),
+            ],
+            zoomPanBehavior: _zoomPanBehavior,
+          ),
+            ),
+            ),
+       ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -42,7 +277,7 @@ class _GraficoLinearState extends State<GraficoLinear> {
       fontSize: 9,
     );
     Widget text;
-    String texto = view?['Data'][value.toString()];
+    String texto = view['Data'][value.toString()];
     text = Text(texto, style: style);
 
     return SideTitleWidget(
@@ -60,7 +295,7 @@ class _GraficoLinearState extends State<GraficoLinear> {
     jsonData1 = await rootBundle.loadString('asset/Empresas_data/${widget.cardIndex}_media_movel.json');
     //String jsonData2 = await rootBundle.loadString('asset/Empresas_data/${widget.cardIndex}_media_movel.json');
     jsonData2 = await rootBundle.loadString('asset/Empresas_data/${widget.cardIndex}_bandas_boll.json');
-    print(widget.cardIndex);
+    //print(widget.cardIndex);
     view = jsonDecode(jsonData);
     view2 = jsonDecode(jsonData1);
     view3 = json.decode(jsonData2);
@@ -68,12 +303,12 @@ class _GraficoLinearState extends State<GraficoLinear> {
 
 
     //print(view);
-    quant = view?['Close'].length;//determinando limites no eixo x do grafico
+    quant = view['Close'].length;//determinando limites no eixo x do grafico
     
     //setando o array com os valores de cotação no grafico
-    for(int a = 0; a < view?['Close'].length; a++){
-      predicaoValor.insert(a,view?['Close'][a.toString()]);
-      chartData.insert(a, FlSpot(a.toDouble(), view?['Close'][a.toString()]));
+    for(int a = 0; a < view['Close'].length; a++){
+      predicaoValor.insert(a,view['Close'][a.toString()]);
+      chartData.insert(a, FlSpot(a.toDouble(), view['Close'][a.toString()]));
       
     }
     
@@ -154,7 +389,7 @@ class _GraficoLinearState extends State<GraficoLinear> {
                         
 
                         return LineTooltipItem(
-                          '${view?['Data'][flSpot.x.toString()]}\n',
+                          '${view['Data'][flSpot.x.toString()]}\n',
                           TextStyle(
                             color: Colors.black,
                             fontWeight: FontWeight.bold,
@@ -190,7 +425,7 @@ class _GraficoLinearState extends State<GraficoLinear> {
 */
       lineBarsData: [
         //cotacoes:
-        LineChartBarData(
+        LineChartBarData( 
           spots: chartData,
           isCurved: false,
           dotData: const FlDotData(
@@ -270,7 +505,7 @@ class _GraficoLinearState extends State<GraficoLinear> {
     );
   }
 }
-
+*/
 
 
 
@@ -410,7 +645,7 @@ class _GraficoLinearState extends State<GraficoLinear> {
   return touchedBarSpots.map((barSpot) {
     final flSpot = barSpot;
     return LineTooltipItem(
-      '${view?['Data'][flSpot.x.toString()]}\n',
+      '${view['Data'][flSpot.x.toString()]}\n',
       TextStyle(
         color: Colors.black,
         fontWeight: FontWeight.bold,
@@ -440,7 +675,7 @@ class _GraficoLinearState extends State<GraficoLinear> {
       fontSize: 9,
     );
     Widget text;
-    String texto = view?['Data'][value.toString()];
+    String texto = view['Data'][value.toString()];
     text = Text(texto, style: style);
 
     return SideTitleWidget(
@@ -466,12 +701,12 @@ class _GraficoLinearState extends State<GraficoLinear> {
 
 
     //print(view);
-    quant = view?['Close'].length;//determinando limites no eixo x do grafico
+    quant = view['Close'].length;//determinando limites no eixo x do grafico
     
     //setando o array com os valores de cotação no grafico
-    for(int a = 0; a < view?['Close'].length; a++){
-      predicaoValor.insert(a,view?['Close'][a.toString()]);
-      chartData.insert(a, FlSpot(a.toDouble(), view?['Close'][a.toString()]));
+    for(int a = 0; a < view['Close'].length; a++){
+      predicaoValor.insert(a,view['Close'][a.toString()]);
+      chartData.insert(a, FlSpot(a.toDouble(), view['Close'][a.toString()]));
       
     }
     
@@ -559,7 +794,7 @@ class _GraficoLinearState extends State<GraficoLinear> {
                       // Linha de dados (cotações)
                       tooltips.add(
                         LineTooltipItem(
-                          '${view?['Data'][flSpot.x.toString()]}\n',
+                          '${view['Data'][flSpot.x.toString()]}\n',
                           TextStyle(
                             color: Colors.black,
                             fontWeight: FontWeight.bold,
@@ -591,7 +826,7 @@ class _GraficoLinearState extends State<GraficoLinear> {
                         final mediaMovelSpot = chartData_media_movel[index];
                         tooltips.add(
                           LineTooltipItem(
-                            '${view?['Data'][mediaMovelSpot.x.toString()]}\n',
+                            '${view['Data'][mediaMovelSpot.x.toString()]}\n',
                             TextStyle(
                               color: Colors.black,
                               fontWeight: FontWeight.bold,
