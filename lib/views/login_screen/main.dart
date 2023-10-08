c5931aadc7daaa5634a85c3b282ad799786a2cd7
Josué Teobaldo <josueteobaldo220@hotmail.com>
2023-10-07 23:25:37 -0300

    feat: alterando algumas coisas

--- lib/views/login_screen/main.dart
+++ lib/views/login_screen/main.dart
@@ -4,7 +4,6 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'package:theras_app/views/menu_empresas/main.dart';
-
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
@@ -89,62 +88,92 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
                 const SizedBox(height: 20),
                 ElevatedButton(
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      final email = _emailController.text;
-                      final password = _passwordController.text;
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        final email = _emailController.text;
+                        final password = _passwordController.text;
 
-                      try {
-                        final userCredential =
-                            await _auth.signInWithEmailAndPassword(
-                          email: email,
-                          password: password,
-                        );
-                        if (userCredential.user != null) {
-                          Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  MenuEmpresas(title: "THERAS"),
-                            ),
+                        try {
+                          final userCredential =
+                              await _auth.signInWithEmailAndPassword(
+                            email: email,
+                            password: password,
                           );
-                        } else {
+                          if (userCredential.user != null) {
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    MenuEmpresas(title: "THERAS"),
+                              ),
+                            );
+                          } else {
+                            setState(() {
+                              _errorMessage = 'Credenciais inválidas';
+                            });
+                          }
+                        } catch (e) {
                           setState(() {
-                            _errorMessage = 'Credenciais inválidas';
+                            _errorMessage = 'Erro durante o login';
                           });
                         }
-                      } catch (e) {
-                        setState(() {
-                          _errorMessage = 'Erro durante o login';
-                        });
                       }
-                    }
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 100, vertical: 20),
-                    child: const Text(
-                      "Login",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
+                    },
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 100, vertical: 20),
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ],
+                    )),
                 ElevatedButton(
-                  onPressed: () async {
-                    // Faça login com o Google ao pressionar este botão
-                    await _handleGoogleSignIn();
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 100,
-                      vertical: 20,
-                    ),
-                    child: const Text(
-                      "Login com o Google",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ),
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        final email = _emailController.text;
+                        final password = _passwordController.text;
+
+                        try {
+                          final userCredential =
+                              await _auth.signInWithEmailAndPassword(
+                            email: email,
+                            password: password,
+                          );
+                          if (userCredential.user != null) {
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    MenuEmpresas(title: "THERAS"),
+                              ),
+                            );
+                          } else {
+                            setState(() {
+                              _errorMessage = 'Credenciais inválidas';
+                            });
+                          }
+                        } catch (e) {
+                          setState(() {
+                            _errorMessage = 'Erro durante o login';
+                          });
+                        }
+                      }
+                    },
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 100, vertical: 20),
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ],
+                    )),
+                const SizedBox(height: 20),
                 if (_errorMessage != null)
                   const Text(
                     "Usuário ou Senha estão Incorretos",
